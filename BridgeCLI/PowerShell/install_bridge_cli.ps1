89718f2cf20dba03ddfe76e79790195eb6cd20b0
smiths@blackduck.com <120398516+snps-steve@users.noreply.github.com>
2025-09-29 11:06:21 -0500

    Update install_bridge_cli.ps1

--- BridgeCLI/PowerShell/install_bridge_cli.ps1
+++ BridgeCLI/PowerShell/install_bridge_cli.ps1
@@ -1,6 +1,6 @@
 # Set variables
-$baseUrl = "https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/bridge/binaries/bridge-cli-thin-client/latest"
-$zipFile = "bridge-cli-thin-client-windows.zip"
+$baseUrl = "https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/bridge/binaries/bridge-cli-thin-client/latest/"
+$zipFile = "bridge-cli-linux64.zip"
 $downloadUrl = "$baseUrl/$zipFile"
 $installDir = "$env:ProgramFiles\BridgeCLI"
 $zipPath = "$env:TEMP\$zipFile"
@@ -11,11 +11,11 @@ if (-not (Test-Path $installDir)) {
 }
 
 # Download the ZIP file
-Write-Host "Downloading Bridge CLI from $downloadUrl..."
+Write-Host "Downloading Bridge CLI for Linux 64 bit OS from $downloadUrl..."
 Invoke-WebRequest -Uri $downloadUrl -OutFile $zipPath
 
 # Extract the ZIP contents
-Write-Host "Extracting Bridge CLI to $installDir..."
+Write-Host "Extracting Bridge CLI for Linux 64 bit OS to $installDir..."
 Expand-Archive -Path $zipPath -DestinationPath $installDir -Force
 # Optionally add to PATH (for current user)
 $envPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
